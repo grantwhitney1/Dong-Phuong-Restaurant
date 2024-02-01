d3a4b4a4735c8d9df8cc2f5c646bbf393ce4eb3f
Grant Whitney <grant.whitney@selu.edu>
2024-02-01 17:06:37 -0600

    Update main.tsx (#26)
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -12,7 +12,6 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
