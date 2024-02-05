46437f3e663e15dc43abcbda12360f8f2c3959ce
Grant Whitney <grant.whitney@selu.edu>
2024-02-05 12:24:06 -0600

    Init (#28)
    * init
    
    * move dockerignore, rebuild
    
    * gitignore
    
    * untracking ignored files
    
    * react app init
    
    * minimizing the frontend
    
    * finishing up program.cs base
    
    * remove unnecessary classes for tests
    
    * untrack .idea
    
    * vs and vscode gitignore
    
    * typescript init
    
    * serving the react app as static files from the api
    
    * Create web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * Update web-build.yml
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    
    * folder structure
    
    * react redux + file structure
    
    * hotfix
    
    * remove redux, remove static files
    
    * regen packages
    
    ---------
    
    Signed-off-by: Grant Whitney <grant.whitney@selu.edu>
    Co-authored-by: Grant Whitney <grant@elevator3.com>

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './state/store'
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    );
-} else {
-    console.error('Failed to find the root element');
-}
