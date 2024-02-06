c07283884d1393c2523e6c94e57922f9f7361713
Grant Whitney <grant.whitney@selu.edu>
2024-02-05 19:48:57 -0600

    Frontend org (#31)
    * ts linting, mui, react modernization
    
    * routing
    
    ---------
    
    Co-authored-by: Grant Whitney <grant@elevator3.com>

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,20 +1,53 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, // This is useful for recognizing global variables like module, require, etc.
+  },
+  parser: '@typescript-eslint/parser', // Use the TypeScript parser for ESLint
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      // This will help with module resolution
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended', // Use the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint', // Include the plugin for TypeScript
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // Add or modify rules as needed
   },
-}
+  overrides: [
+    {
+      // Enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off', // TypeScript's compiler already checks for undefined variables
+      },
+    },
+  ],
+};
