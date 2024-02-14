d95b5322e3df06ebb12177a37eb4c642dfc054fe
Grant Whitney <grant@elevator3.com>
2024-02-14 13:48:12 -0600

    DbContext Init, Local and Cloud

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,4 +1,25 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using DongPhuong.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var localDbConnectionString = builder.Configuration.GetConnectionString("LocalDbConnectionString");
+
+var vaultUrl = builder.Configuration["VaultUrl"];
+var client = new SecretClient(vaultUri: new Uri(vaultUrl!), credential: new DefaultAzureCredential());
+var cloudDbConnectionString = client.GetSecret("DbConnectionString");
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(localDbConnectionString));
+} else if (builder.Environment.IsProduction()) 
+{
+    builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString(cloudDbConnectionString.Value.Value)));
+}
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
