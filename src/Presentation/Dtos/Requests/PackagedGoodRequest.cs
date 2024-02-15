eafdae78b9ccc786b5450646bb980d99d5facce7
inmwild <89433205+inmwild@users.noreply.github.com>
2024-02-14 22:21:56 -0600

    created Packagedgood class and dto (#51)

---
+++ src/Presentation/Dtos/Requests/PackagedGoodRequest.cs
@@ -0,0 +1,8 @@
+using DongPhuong.Application.Dtos;
+
+namespace DongPhuong.Presentation.Dtos.Requests;
+
+public class PackagedGoodRequest
+{
+    public PackagedGoodCommandDto PackagedGoodCommandDto { get; set; }
+}
---
+++ src/Presentation/Dtos/Responses/PackagedGoodResponse.cs
@@ -0,0 +1,8 @@
+using DongPhuong.Application.Dtos;
+
+namespace DongPhuong.Presentation.Dtos.Responses;
+
+public class PackagedGoodResponse
+{
+    public PackagedGoodQueryDto PackagedGoodQueryDto { get; set; }
+}
