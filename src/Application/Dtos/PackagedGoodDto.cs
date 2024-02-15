a4489e9e41a0607226f6d5c2bfc72aae3a85abde
Grant Whitney <grant.whitney@selu.edu>
2024-02-14 22:56:32 -0600

    created Packagedgood class and dto (#53)
    * created Packagedgood class and dto
    
    * created Packagedgood class and dto
    
    ---------
    
    Co-authored-by: inmwild <matthew.wilder@selu.edu>

---
+++ src/Application/Dtos/PackagedGoodDto.cs
@@ -0,0 +1,19 @@
+namespace DongPhuong.Application.Dtos;
+
+public class PackagedGoodDto : PackagedGoodQueryDto
+{
+    
+}
+
+public class PackagedGoodQueryDto : PackagedGoodCommandDto
+{
+    public int Id { get; set; }
+}
+
+public class PackagedGoodCommandDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ src/Domain/Entities/PackagedGood.cs
@@ -0,0 +1,8 @@
+using DongPhuong.Application.Dtos;
+
+namespace DongPhuong.Domain.Entities;
+
+public class PackagedGood : PackagedGoodDto
+{
+    
+}
