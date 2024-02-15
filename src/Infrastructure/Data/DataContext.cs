5f55629b80a18e8f2c2dae72b7a0c076e1e5f020
Grant Whitney <grant@elevator3.com>
2024-02-15 14:27:04 -0600

    Correct version, globally install packages

--- src/Infrastructure/Data/DataContext.cs
+++ src/Infrastructure/Data/DataContext.cs
@@ -1,5 +1,9 @@
+using DongPhuong.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DongPhuong.Infrastructure.Data;
 
-public class DataContext(DbContextOptions<DataContext> options) : DbContext(options);
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public virtual required DbSet<PackagedGood> PackagedGoods { get; init; }
+}
