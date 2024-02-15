3c5aa7e5bfc779652091075edf3da0c244172f45
Grant Whitney <grant@elevator3.com>
2024-02-14 23:23:19 -0600

    Update PackagedGood.cs

--- src/Domain/Entities/PackagedGood.cs
+++ src/Domain/Entities/PackagedGood.cs
@@ -1,8 +1,9 @@
 using DongPhuong.Application.Dtos;
+using DongPhuong.Domain.Interfaces.Data;
 
 namespace DongPhuong.Domain.Entities;
 
-public class PackagedGood : PackagedGoodDto
+public class PackagedGood : PackagedGoodDto, IEntity
 {
     
 }
