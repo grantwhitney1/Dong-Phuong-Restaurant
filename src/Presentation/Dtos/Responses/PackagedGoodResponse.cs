6fe113bc9a16d4935f662e2f44f61bf492436786
Grant Whitney <grant.whitney@selu.edu>
2024-02-14 22:27:36 -0600

    Revert "created Packagedgood class and dto (#51)" (#52)
    This reverts commit eafdae78b9ccc786b5450646bb980d99d5facce7.

--- src/Presentation/Dtos/Responses/PackagedGoodResponse.cs
+++
@@ -1,8 +0,0 @@
-using DongPhuong.Application.Dtos;
-
-namespace DongPhuong.Presentation.Dtos.Responses;
-
-public class PackagedGoodResponse
-{
-    public PackagedGoodQueryDto PackagedGoodQueryDto { get; set; }
-}
