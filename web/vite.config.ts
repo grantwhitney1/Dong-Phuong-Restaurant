754c7fb8d3729a2b4015dcdf4b75c45815b8d91e
Grant Whitney <grant@elevator3.com>
2024-02-29 13:28:28 -0600

    wip

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,11 @@
 import {defineConfig, loadEnv} from "vite";
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-    // vite config
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
-      VITE_API_BASE_URL: env.APP_ENV === 'development' ? 'https://localhost:7217/' : 'https://dong-phuong.azurewebsites.net',
+      VITE_API_BASE_URL: JSON.stringify(JSON.stringify(env.APP_ENV === 'development' ? 'https://localhost:7217/' : 'https://dong-phuong.azurewebsites.net')),
     },
   }
-})
+});
