ef13fa6328a5eb06e09491d846d8eecbb8305a28
Cchris7 <85213942+Cchris7@users.noreply.github.com>
2024-03-20 12:50:57 -0500

    Seed preparedgoods

--- src/Domain/Entities/Features/PreparedGoods/PreparedGood.cs
+++ src/Domain/Entities/Features/PreparedGoods/PreparedGood.cs
@@ -8,4 +8,11 @@ public class PreparedGood : IEntity
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal? Price { get; set; }
+
+    public PreparedGood(string name, string description, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
 }
