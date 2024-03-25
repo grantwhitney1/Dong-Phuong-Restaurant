d5bc7064bacca974273ab631a2c5eda37ded6822
Cchris7 <85213942+Cchris7@users.noreply.github.com>
2024-03-24 20:26:28 -0500

    Seed Packaged Goods

--- src/Domain/Entities/Features/PackagedGoods/PackagedGood.cs
+++ src/Domain/Entities/Features/PackagedGoods/PackagedGood.cs
@@ -9,4 +9,12 @@ public class PackagedGood : IEntity
     public string? Description { get; set; }
     public decimal? Weight { get; set; }
     public decimal? Price { get; set; }
+
+    public PackagedGood(string name, string description, decimal weight, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Weight = weight;
+        Price = price;
+    }
 }
