48c36b0315f3b42dcb0992239cab0e4a6ea1d87a
Grant Whitney <grant@elevator3.com>
2024-04-11 09:47:05 -0500

    corrected api url

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,9 @@
 import {defineConfig} from "vite";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
   return {
     define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(mode  === 'development' ? 'https://localhost:7217/' : 'https://dong-phuong.azurewebsites.net/'),
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(mode === 'development' ? 'https://localhost:7217/' : 'https://dong-phuong-api.azurewebsites.net/'),
     },
   }
 });
